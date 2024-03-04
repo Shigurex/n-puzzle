eae9c44eeecdd4aa559d9634dc89a9239adc86d4
Yamato Hokari <yamatohokari@gmail.com>
2024-03-04 23:05:26 +0900

    web added

--- test/yahokari/srcs/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <Board.hpp>
-
-int main(void)
-{
-	Board board(4, {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}});
-
-	std::cout << "Board is solved: " << (board.isSolved() ? "TRUE" : "FALSE") << std::endl;
-	return (0);
-}
