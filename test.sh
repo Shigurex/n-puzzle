dd8521fb5627a69e8cf8dddffc495905d6ad04b5
Yamato Hokari <yamatohokari@gmail.com>
2024-03-15 19:38:13 +0900

    simple test script created

---
+++ test.sh
@@ -0,0 +1,29 @@
+make
+echo "Error: No solution" > output.error.txt
+for i in {1..10}
+do
+	python test/yahokari/npuzzle-gen.py 3 -u > input.txt && ./n_puzzle ./input.txt &> output.txt
+	diff output.error.txt output.txt > /dev/null
+	if [ $? -eq 0 ]; then
+		echo "Test $i: OK"
+	else
+		echo "Test $i: KO"
+	fi
+
+	rm input.txt output.txt
+done
+
+for i in {1..10}
+do
+	python test/yahokari/npuzzle-gen.py 3 -s > input.txt && ./n_puzzle ./input.txt &> output.txt
+	diff output.error.txt output.txt > /dev/null
+	if [ $? -ne 0 ]; then
+		echo "Test $i: OK"
+	else
+		echo "Test $i: KO"
+	fi
+
+	rm input.txt output.txt
+done
+
+rm output.error.txt
